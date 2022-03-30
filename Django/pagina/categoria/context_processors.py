00b83f391ff316f94f19553786c57e11d68c32d0
leonely1990 <43575777+leonely1990@users.noreply.github.com>
2022-03-29 20:16:40 -0400

    Delete Django directory

--- Django/pagina/categoria/context_processors.py
+++
@@ -1,6 +0,0 @@
-from . import models
-
-
-def menu_link(request):
-    links = models.Categoria.objects.all()
-    return dict(links=links)
--- Django/pagina/store/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.store, name='store'),
-    path('<slug:category_slug>', views.store, name='slug_categoria'),
-]
